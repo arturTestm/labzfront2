893f734730422a29d5beec53710280a92931052e
Arthur <arthur@testm.com>
2020-05-26 09:37:55 +0300

    Connect state in localStorage.js

---
+++ src/Store/localStorage.js
@@ -0,0 +1,25 @@
+/**
+ * Load state from localstorage
+ * */
+export const loadState = () => {
+    try {
+        const serializedState = localStorage.getItem('state');
+        if (serializedState === null) {
+            return undefined;
+        }
+        return JSON.parse(serializedState);
+    } catch (err) {
+        return undefined;
+    }
+};
+
+/**
+ * Save state in localstorage
+ * */
+export const saveState = (state) => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('state', serializedState);
+    } catch {
+    }
+};
--- src/Store/store.js
+++ src/Store/store.js
@@ -1,6 +1,14 @@
 import {createStore} from "redux";
 import rootReducers from "./Reducers";
+import {saveState, loadState} from "./localStorage";
+const throttle = require('lodash.throttle');
 
-const store = createStore(rootReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(rootReducers, loadState(), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+store.subscribe(throttle(() => {
+    saveState({
+        counter: store.getState().counter,
+    })
+}, 1000));
 
 export default store;
